e72f830fe74aaa389a8aa6855f5ea2959ed10926
Lekha - Perumal <leks.be@gmail.com>
2020-09-12 00:07:57 +0530

    Initial Commit

---
+++ src/helloWorldJenkins/HelloWorld.java
@@ -0,0 +1,10 @@
+package helloWorldJenkins;
+
+public class HelloWorld {
+	
+	public static void main(String args[]) {
+	
+	System.out.println("This is to build a Java project in Jenkins");
+
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Lekha.Perumal
+ *
+ */
+module helloWorldJenkins {
+}
